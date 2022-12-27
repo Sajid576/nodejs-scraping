9a6b4f31eed21b9d034210a0cadbbadb01aa1210
Sajid576 <sajidahmed696@gmail.com>
2022-12-27 11:30:24 +0600

    Add proxy template with axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,39 @@
 import axios from 'axios';
 
+// const proxy_list = [
+//   {
+//     protocol: 'http',
+//     host: '149.129.239.170',
+//     port: 8080,
+//   },
+
+//   {
+//     protocol: 'http',
+//     host: '132.129.121.148',
+//     port: 8080,
+//   },
+//   {
+//     protocol: 'http',
+//     host: '154.129.98.156',
+//     port: 8080,
+//   },
+//   {
+//     protocol: 'http',
+//     host: '211.129.132.150',
+//     port: 8080,
+//   },
+//   {
+//     protocol: 'http',
+//     host: '164.129.114.111',
+//     port: 8080,
+//   },
+// ];
+// let random_index = Math.floor(Math.random() * proxy_list.length);
+
 export async function GET(baseUrl: string, path: string) {
   const { data, status } = await axios.get(baseUrl + path, {
+    // proxy: proxy_list[random_index],
+
     headers: {
       Accept: 'application/json',
     },
